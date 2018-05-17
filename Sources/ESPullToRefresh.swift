44956b8b963b811140a04c6576953ac1e585a08a
Vitalii Hudenko <vitaliy.gudenko@gmail.com>
2018-05-17 10:52:25 +0300

    wip

--- Sources/ESPullToRefresh.swift
+++ Sources/ESPullToRefresh.swift
@@ -469,7 +469,7 @@ open class ESRefreshFooterView: ESRefreshComponent {
         // http://stackoverflow.com/questions/2037892/stop-deceleration-of-uiscrollview
         if scrollView.isDecelerating {
             var contentOffset = scrollView.contentOffset
-            contentOffset.y = min(contentOffset.y, scrollView.contentSize.height - scrollView.frame.size.height)
+            contentOffset.y = min(contentOffset.y, scrollView.contentSize.height - scrollView.frame.size.height - scrollViewInsets.bottom)
             if contentOffset.y < 0.0 {
                 contentOffset.y = 0.0
                 UIView.animate(withDuration: 0.1, animations: { 
